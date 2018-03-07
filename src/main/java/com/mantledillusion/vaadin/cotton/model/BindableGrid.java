e7bd6da2d8886fe37dce9fe8c21a4b45d5da151c
tobias.weber <tobias.weber@ecotel.de>
2018-03-07 13:30:39 +0100

    Added convenience methods for adding simple property displaying columns

--- src/main/java/com/mantledillusion/vaadin/cotton/model/BindableGrid.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/model/BindableGrid.java
@@ -477,6 +477,84 @@ public final class BindableGrid<RowType, ModelType> extends Composite {
 	private int columnCounter = 0;
 
 	/**
+	 * Adds a column to this {@link BindableGrid} that is bound to the given
+	 * property.
+	 * 
+	 * @param <PropertyType>
+	 *            The type of the property to bind.
+	 * @param property
+	 *            The property to bind the column to; might <b>not</b> be null.
+	 * @return A {@link ModelPropertyColumnConfiguration} to configure the
+	 *         registration of the given column in this grid with.
+	 */
+	public <PropertyType> ModelPropertyColumnConfiguration<RowType> addColumn(
+			ModelProperty<ModelType, PropertyType> property) {
+		return addColumn(rowAccessor -> rowAccessor.bindLabelForProperty(property));
+	}
+
+	/**
+	 * Adds a column to this {@link BindableGrid} that is bound to the given
+	 * property.
+	 * 
+	 * @param <PropertyType>
+	 *            The type of the property to bind.
+	 * @param property
+	 *            The property to bind the column to; might <b>not</b> be null.
+	 * @param renderer
+	 *            The {@link StringRenderer} to render the bound property with;
+	 *            might <b>not</b> be null.
+	 * @return A {@link ModelPropertyColumnConfiguration} to configure the
+	 *         registration of the given column in this grid with.
+	 */
+	public <PropertyType> ModelPropertyColumnConfiguration<RowType> addColumn(
+			ModelProperty<ModelType, PropertyType> property, StringRenderer<PropertyType> renderer) {
+		return addColumn(rowAccessor -> rowAccessor.bindLabelForProperty(property, renderer));
+	}
+
+	/**
+	 * Adds a column to this {@link BindableGrid} that is bound to the given
+	 * property.
+	 * 
+	 * @param <PropertyType>
+	 *            The type of the property to bind.
+	 * @param property
+	 *            The property to bind the column to; might <b>not</b> be null.
+	 * @param configurator
+	 *            The configurator to use on the given columns registration; might
+	 *            be null.
+	 * @return A {@link ModelPropertyColumnConfiguration} to configure the
+	 *         registration of the given column in this grid with.
+	 */
+	public <PropertyType> ModelPropertyColumnConfiguration<RowType> addColumn(
+			ModelProperty<ModelType, PropertyType> property, ModelPropertyColumnConfigurator<RowType> configurator) {
+		return addColumn(rowAccessor -> rowAccessor.bindLabelForProperty(property), configurator);
+	}
+
+	/**
+	 * Adds a column to this {@link BindableGrid} that is bound to the given
+	 * property.
+	 * 
+	 * @param <PropertyType>
+	 *            The type of the property to bind.
+	 * @param property
+	 *            property The property to bind the column to; might <b>not</b> be
+	 *            null.
+	 * @param renderer
+	 *            The {@link StringRenderer} to render the bound property with;
+	 *            might <b>not</b> be null.
+	 * @param configurator
+	 *            The configurator to use on the given columns registration; might
+	 *            be null.
+	 * @return A {@link ModelPropertyColumnConfiguration} to configure the
+	 *         registration of the given column in this grid with.
+	 */
+	public <PropertyType> ModelPropertyColumnConfiguration<RowType> addColumn(
+			ModelProperty<ModelType, PropertyType> property, StringRenderer<PropertyType> renderer,
+			ModelPropertyColumnConfigurator<RowType> configurator) {
+		return addColumn(rowAccessor -> rowAccessor.bindLabelForProperty(property, renderer), configurator);
+	}
+
+	/**
 	 * Adds the given {@link ModelPropertyColumn} to this {@link BindableGrid}.
 	 * 
 	 * @param column
