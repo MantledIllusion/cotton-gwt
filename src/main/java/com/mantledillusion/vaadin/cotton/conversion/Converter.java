b85e72761b0bddc63e2ea8d0195da58da7b991af
tobias.weber <tobias.weber@ecotel.de>
2018-07-04 15:18:47 +0200

    Removed converters again; maybe they will become an own artefact

--- src/main/java/com/mantledillusion/vaadin/cotton/conversion/Converter.java
+++
@@ -1,6 +0,0 @@
-package com.mantledillusion.vaadin.cotton.conversion;
-
-public interface Converter<SourceType, TargetType> {
-
-	TargetType toTarget(SourceType source, ConversionService service) throws Exception;
-}
